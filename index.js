b42fcf5a57ae41bf25768184d770ae8769e30e45
Mateus Sousa <129328490+mateusousadeoliveira@users.noreply.github.com>
2026-09-02 21:50:11 +0000

    script movido para index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function buscarJogo() {
+    
+    const termo = document.getElementById('campoPesquisa').value.toLowerCase().trim();
 
+   
+    const paginas = {
+        "minecraft": "jogos/minecraft.html",
+        "god of war": "jogos/godofwar.html",
+        "batman arkham night": "jogos/batmanarkhamnight.html",
+    };
+
+    
+    if (paginas[termo]) {
+        
+        window.location.href = paginas[termo];
+    } else {
+        
+        alert("Jogo não encontrado ou disponível.");
+    }
+}
